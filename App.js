cdbdfe0055f401bda64f880e0b5e193884d66dfc
heemozzi <kkk4698@naver.com>
2021-02-10 16:56:54 +0900

    react-navigation

--- App.js
+++ App.js
@@ -1,39 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-// from 자바스크립트 파일명
-// from 디렉토리명
-// import HelloWorld from './components/HelloWorld';
-//import Counter from './components/Counter'
-//import LotsOfGreetings from './components/LostsOfGreetings'
-import SimpleList from './components/SimpleList'
-/* App 컴포넌트 시작 */
-export default function App() {
-  // JSX
-  // UI 템플릿을 작성
+
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+import Home from './components/Home'
+import List from './components/List'
+import Actions from './components/Actions'
+import Details from './components/Details'
+
+// https://ionicons.com/
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const Tab = createBottomTabNavigator();
+const ListStack = createStackNavigator();
+const HomeStack = createStackNavigator();
+
+const HomeStackScreen = () => {
   return (
-          // 속성={값}
-          // 속성={"문자열"}
-          // 속성={{name:"sss", phone:"aaa"}}
-          // 속성={객체변수}
-    <View style={styles.container}>
-      {/* <HelloWorld></HelloWorld> */}
-      {/* <Counter></Counter> */}
-      <SimpleList></SimpleList>
-      {/* <LotsOfGreetings></LotsOfGreetings> */}
-      <StatusBar style="auto" />
-    </View>
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={Home} options={{title:"Home", headerTitleAlign:"center"}} />
+      <HomeStack.Screen name="Details" component={Details} options={{title:"Details", headerTitleAlign:"center"}}  />
+    </HomeStack.Navigator>
+  )
+}
+
+const ListStackScreen = () => {
+  return (
+    <ListStack.Navigator>
+      <ListStack.Screen name="List" component={List} options={{title:"List", headerTitleAlign:"center"}} />
+      <ListStack.Screen name="Details" component={Details} options={{title:"Details", headerTitleAlign:"center"}}  />
+    </ListStack.Navigator>
+  )
+}
+
+const screeOptions = ({ route }) => ({
+  tabBarIcon: ({ focused, color, size }) => {
+    let iconName;
+
+    switch(route.name){
+      case 'Home':
+        iconName = focused
+          ? 'home'
+          : 'home-outline';
+        break;
+      case 'List':
+        iconName = focused
+          ? 'list'
+          : 'list-outline'; 
+        break;
+      case 'Actions':
+        iconName = focused
+          ? 'checkmark'
+          : 'checkmark-outline'; 
+        break;
+    }
+
+    // You can return any component that you like here!
+    return <Ionicons name={iconName} size={size} color={color} />;
+  },
+})
+
+const tabBarOptions= {
+  activeTintColor: 'tomato',
+  inactiveTintColor: 'gray',
+}
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+     <NavigationContainer>
+       <Tab.Navigator tabBarOptions={tabBarOptions} screenOptions={screeOptions}>
+          <Tab.Screen name="Home" component={HomeStackScreen} />
+          <Tab.Screen name="List" component={ListStackScreen} />
+          <Tab.Screen name="Actions" component={Actions} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+
+  
   );
 }
-/* App 컴포넌트 끝 */
-
-// style="dispaly:flex;"
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
